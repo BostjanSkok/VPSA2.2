392a235d4b6f55e83b469bd5dcdedc7eb7b917e7
BostjanSkok <BostjanSkok2@gmail.com>
2013-11-05 23:27:45 +0100

    First try

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <windows.h>
+
+pthread_mutex_t read, write;
+const int N = 10;
+const int writters = 2;
+
+int numOfReads = 0;
+
+
+
+void *pisatelj(void* n)
+{
+	pthread_mutex_lock(&write);
+	printf("P%d:Z \n", (int)n);
+	Sleep(2 * (int)n);
+	printf("P%d:K \n", (int)n);
+	pthread_mutex_unlock(&read);
+	return NULL;
+}
+
+
+void*bralec(void *n)
+{
+	printf("B%d:Z \n", (int)n);
+	Sleep((int)n);
+	printf("B%d:K \n", (int)n);
+	pthread_mutex_lock(&read);
+	/*if (numOfReads < N){
+		numOfReads++;
+		pthread_mutex_unlock(&read);
+	}else*/
+	 pthread_mutex_unlock(&write);
+	return NULL;
+}
+
+int main() {
+
+	pthread_t threads[N];
+	pthread_mutex_init(&read, NULL);
+	pthread_mutex_init(&write, NULL);
+	pthread_mutex_lock(&read); // Nothing to read
+
+	for (int i = 0; i < N; i++)
+		pthread_create(&threads[i],NULL, i<2?pisatelj:bralec,(void *)i);
+
+	for (int i = 0; i < N; i++)
+		pthread_join(threads[i],NULL);
+	
+
+
+	pthread_mutex_destroy(&read);
+	pthread_mutex_destroy(&write);
+
+	return EXIT_SUCCESS;
+
+}
+
